50a43ae6c79f85c5a483c49608b7d2739c4a63ae
Rubal-code <rubal3232@gmail.com>
2026-08-18 14:55:44 +0530

    session management using the redis

--- backend/services/auth/controllers/auth.controller.js
+++ backend/services/auth/controllers/auth.controller.js
@@ -2,6 +2,7 @@ import { getAuth } from "firebase-admin/auth"
 import User from "../models/user.model.js"
 import { app } from "../config/firebase.js"
 import crypto from "crypto"
+import redis from "../../../shared/redis/redis.js"
 
 export const login = async (req, res) => {
     try {
@@ -27,6 +28,12 @@ export const login = async (req, res) => {
         }
 
         const sessionId = crypto.randomUUID()
+        await redis.set(`session-${sessionId}` , JSON.stringify({
+            userId:user._id,
+            name:user.name,
+            email:user.email,
+            avatar:user.avatar
+        }),"EX",7*24*60*60)
 
         res.cookie("sessionId", sessionId, {
             httpOnly: true,
@@ -42,6 +49,39 @@ export const login = async (req, res) => {
 
         return res.status(500).json({
             message: `login error ${error.message}`
+        })
+    }
+}
+
+
+export const logOut = async(req,res) => {
+    try{
+        const sessionId=req.cookies?.session
+        await redis.del(`session-${sessionId}`)
+        res.clearCookie("sessionId")
+        return res.status(200).json({
+            success:true,
+            message:"Logged out successfully"
+        })
+    }
+    catch(error){
+        console.error("LOGOUT ERROR:", error)
+
+        return res.status(500).json({
+            message: `logout error ${error.message}`
+        })
+    }
+}
+
+export const getCurrentUser = async(req,res)=>{
+    try{
+        
+    }
+    catch(error){
+        console.error("LOGOUT ERROR:", error)
+
+        return res.status(500).json({
+            message: `logout error ${error.message}`
         })
     }
 }
